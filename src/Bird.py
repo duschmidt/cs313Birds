b18e96125f2dab16665d36436ff4e9552e80e674
khiner <karl.hiner@gmail.com>
2012-10-02 02:06:55 -0700

    refactoring x, y into Position class with row/col, refactoring Bird.move()

--- src/Bird.py
+++ src/Bird.py
@@ -1,4 +1,5 @@
 import MapFrame
+
 import random as rand
 
 class Bird:
@@ -6,48 +7,21 @@ class Bird:
     
     def __init__(self, map):
         self.map = map
-        self.x = rand.randint(0, map.numCols - 1)
-        self.y = rand.randint(0, map.numRows - 1)
+        self.pos = MapFrame.Position(rand.randint(0, map.numCols - 1), rand.randint(0, map.numRows - 1))
 
     def move(self):
-        # find the diffusion values neighboring bird
         neighborValues = dict()
-        neighborValues['N'] = self.map.npAry[self.x, (self.y - 1) % self.map.numRows]
-        neighborValues['S'] = self.map.npAry[self.x, (self.y + 1) % self.map.numRows]
-        neighborValues['E'] = self.map.npAry[(self.x + 1) % self.map.numCols, self.y]
-        neighborValues['W'] = self.map.npAry[(self.x - 1) % self.map.numCols, self.y]
+        # find the diffusion values neighboring the bird
+        for direction in ('N', 'S', 'E', 'W'):
+            neighborValues[direction] = self.map.valueInDirection(self.pos, direction)
 
         # find direction with the max diffusion value
-        while True:
-            direction = max(neighborValues, key=neighborValues.get)
-            neighborValues.pop(direction)
-            newCell = self.cellInDirection(direction)
-            # if a new, unoccupied cell has been found, or if we're out of directions to check, break
-            if newCell != None or not neighborValues:
-                break
-
-        if newCell != None:
-            self.x = newCell[0]
-            self.y = newCell[1]
+        direction = max(neighborValues, key=neighborValues.get)
+        # move to new position if it's not occupied with a bird (value of -1)
+        if neighborValues[direction] != -1:
+            self.pos = self.map.posInDirection(self.pos, direction)
             
         # if bird got to goal, remove goal
-        if [self.x, self.y] in self.map.goals:
-            self.map.goals.remove([self.x, self.y])
-        
-    def cellInDirection(self, direction):
-        cell = [self.x, self.y]
-        if direction == 'N':
-            cell[1] -= 1
-        elif direction == 'S':
-            cell[1] += 1
-        elif direction == 'E':
-            cell[0] += 1
-        else:
-            cell[0] -= 1
-            
-        # wrap bird loc around grid
-        cell[0] %= self.map.numCols
-        cell[1] %= self.map.numRows
-
-        # check if another bird is in the new cell
-        return None if self.map.occupied(cell[0], cell[1]) else cell
+        if self.pos in self.map.goals:
+            self.map.goals.remove(self.pos)
+    
--- src/MapFrame.py
+++ src/MapFrame.py
@@ -3,8 +3,19 @@ import Tkinter as tkinter
 import numpy as np
 import scipy.signal as sig
 import random as rand
-import Bird
+from Bird import Bird
 
+class Position:
+    col = 0
+    row = 0
+    
+    def __init__(self, col, row):
+        self.col = col
+        self.row = row
+
+    def __eq__(self, other):
+        return self.col == other.col and self.row == other.row
+        
 class MapFrame(Frame):
         # width / height of canvas in pixels
 	canvasWidth = 400
@@ -36,7 +47,7 @@ class MapFrame(Frame):
 
                 self.initGoals()
                 for i in range(10):
-                    self.birds.append(Bird.Bird(self))
+                    self.birds.append(Bird(self))
                 
 	def createWidgets(self):
 		self.menubar = Menu(self.master)
@@ -54,40 +65,63 @@ class MapFrame(Frame):
 		self.Surface.grid(row=0,column=0)
 		self.Surface.bind("<Button-1>", self.leftClick)
 
-        def occupied(self, x, y):
+        def occupied(self, pos):
             """Returns true if a bird is occupying the cell at x, y.  Returns false otherwise"""
             for bird in self.birds:
-                if bird.x == x and bird.y == y:
+                if bird.pos == pos:
                     return True
             return False
             
 	def dummy(self):
 		pass
 
-        def pointToCell(self, x, y):
+        def posInDirection(self, pos, direction):
+            newPos = Position(pos.col, pos.row)
+            
+            if direction == 'N':
+                newPos.row -= 1
+            elif direction == 'S':
+                newPos.row += 1
+            elif direction == 'E':
+                newPos.col += 1
+            else:
+                newPos.col -= 1
+            
+            # wrap bird loc around grid
+            newPos.col %= self.numCols
+            newPos.row %= self.numRows
+
+            # check if another bird is in the new cell
+            return newPos
+
+        def valueInDirection(self, pos, direction):
+            pos = self.posInDirection(pos, direction)
+            return -1 if self.occupied(pos) else self.npAry[pos.col, pos.row]
+            
+        def pointToPos(self, x, y):
             """Convert a pixel x, y point to a cell row, col pair"""
-            return [(int)(x / self.cellWidth), (int)(y / self.cellHeight)]
+            return Position((int)(x / self.cellWidth), (int)(y / self.cellHeight))
             
         def leftClick(self, event):
             """Left click adds/removes goal objects from the grid"""
-            [x, y] = self.pointToCell(event.x, event.y)
-            if [x, y] in self.goals:
-                self.goals.remove([x, y])
+            pos = self.pointToPos(event.x, event.y)
+            if pos in self.goals:
+                self.goals.remove(pos)
             else:
-                self.goals.append([x, y])
+                self.goals.append(pos)
 
         def initGoals(self):
             """Populate the map randomly with goals"""
             for x in xrange(self.numCols):
                 for y in xrange(self.numRows):
                     if rand.random() < .01:
-                        self.goals.append([x, y])
+                        self.goals.append(Position(x, y))
 
         def seedDiffusion(self):
             """Set all cells which hold goals to a max diffusion value of 1 and all other cells to 0"""
             self.npAry.fill(0)
-            for goal in self.goals:
-                self.npAry[goal[0], goal[1]] = 1
+            for pos in self.goals:
+                self.npAry[pos.col, pos.row] = 1
                 
         #Adapted from http://stackoverflow.com/questions/8102781/efficiently-doing-diffusion-on-a-2d-map-in-python
         def diffuse(self, numDiffusions):
@@ -100,18 +134,18 @@ class MapFrame(Frame):
                         if max != 0:
                                 self.npAry /= np.max(self.npAry)
             
-	def getColor(self, x, y):
-                if [x, y] in self.goals:
+	def getColor(self, pos):
+                if pos in self.goals:
                     return "#FF0000"
                 else:
-                    return "#%02XFFFF" % int(abs(255 * (1 - self.npAry[x,y])))
+                    return "#%02XFFFF" % int(abs(255 * (1 - self.npAry[pos.col, pos.row])))
 
-        def drawCell(self, x, y, color):
+        def drawCell(self, pos, color):
                 """Draw a single cell at pos (x, y), filled with specified color"""
-                x1 = x * self.cellWidth
-                y1 = (y + 1) * self.cellHeight
-                x2 = (x + 1) * self.cellWidth
-                y2 = y * self.cellHeight
+                x1 = pos.col * self.cellWidth
+                y1 = (pos.row + 1) * self.cellHeight
+                x2 = (pos.col + 1) * self.cellWidth
+                y2 = pos.row * self.cellHeight
                 self.Surface.create_rectangle(x1, y1, x2, y2, fill=color)
                 
 	def drawGrid(self):
@@ -119,19 +153,20 @@ class MapFrame(Frame):
 		self.Surface.delete(tkinter.ALL)
 		for x in xrange(self.numCols):
 			for y in xrange(self.numRows):
-                                self.drawCell(x, y, self.getColor(x, y))
+                            pos = Position(x, y)   
+                            self.drawCell(pos, self.getColor(pos))
 
         def drawBirds(self):
             for bird in self.birds:
                 bird.move()
-                self.drawCell(bird.x, bird.y, bird.color)
+                self.drawCell(bird.pos, bird.color)
                 
         def drawFrame(self):
             """Main draw method, to be called every frame"""
             #self.Surface.after(self.deltaT)
             # randomly generate a goal from time to time
             if rand.random() < .2:
-                self.goals.append([rand.randint(0, self.numCols - 1), rand.randint(0, self.numRows - 1)])
+                self.goals.append(Position(rand.randint(0, self.numCols - 1), rand.randint(0, self.numRows - 1)))
             self.seedDiffusion()
             self.diffuse(numDiffusions=20)
             self.drawGrid()
