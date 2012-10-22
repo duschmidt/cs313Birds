ed552310ed4c39b6afe568f3d55586af9fc5418d
khiner <karl.hiner@gmail.com>
2012-10-21 17:10:15 -0700

    added inner 'Metric' class to GameState, changed GameState.__init__ to take a map file as an argument to instantiate obstacle array, changed EntityGroup.__init__ to take a list of metrics, added 'diffuse' as a Metric method, and implemented various methods in GameState and EntityGroup

--- src/Birds/EntityGroup.py
+++ src/Birds/EntityGroup.py
@@ -1,41 +1,40 @@
 from pygame.sprite import Group
+from GameState import Metric
 
 class EntityGroup(Group):
 	"""This class manages a group of entities and their diffused environment metrics"""
-
+                
 	groupName = None		#:A name for this group
 	gameState = None		#:A reference to the master gameState object
-	updatedEntities = None	#:A group that will contain updated entities
-	diffusionRates = {}		#:dictionary of diffusion rates keyed by metric name
-	metricArrays = {}		#:dictionary of environment metric arrays keyed by metric name
+        updatedEntities = None          #:A group that will contain updated entities
+	metrics = {}		        #:dictionary of environment metrics (holding metric names, arrays and rates),
+                                        # keyed by metric name
 
-	def __init__(self, gameState, diffRate, groupName):
+	def __init__(self, gameState, groupName, metrics):
 		Group.__init__(self)			#initialize base class
 		self.gameState = gameState
-		self.diffusionRate = diffRate
 		self.groupName = groupName
 		self.updatedEntities = Group()
+                for metric in metrics:
+                        self.metrics[metric.name] = metric
 
 	def update(self):
 		"""Updates member entities then applies diffusion to metric arrays"""
 		Group(self).update() #call the update method for the base Group class
-
-		#TODO Diffuse metric arrays
-		pass
+		#Diffuse metric arrays
+                for metric in self.metrics:
+                    metric.diffuse()
 
 	def getUpdatedEntities(self):
-		pass
+		pass # TODO
 
 	def addEntity(self, entity):
-		Group(self).add(entity)#call the base class add method
-		pass
+		Group(self).add(entity) #call the base class add method
 
 	def removeEntity(self, entity):
 		"""Remove the given entity from this group"""
 		Group(self).remove(entity) #call the base class remove method
-		pass
 
 	def getMetricNames(self):
 		"""Returns a list of names for metrics maintained by this group"""
-		pass
-
+		return self.metrics.keys()
--- src/Birds/GameState.py
+++ src/Birds/GameState.py
@@ -1,14 +1,50 @@
+import numpy as np
 from EntityGroup import EntityGroup
 from Entity import Entity
 
 class GameState():
 	"""This is the top level class responsible for managing game state"""
-	entityGroups={}			#:dictionary of entity groups keyed by group name
+	entityGroups={}		#:dictionary of entity groups keyed by group name
 	obstacleArray = None	#:A numpy array for storing diffusion obstacles
+        numCols = 0
+        numRows = 0
 
-	def __init__(self):
+        class Metric:
+                """Class to hold name, rate and numpy array for a diffusion metric"""
+                def __init__(self, gameState, name, rate, numDiffusions):
+                        self.gameState = gameState
+                        self.name = name
+                        self.rate = rate
+                        self.numDiffusions = numDiffusions
+                        self.array = np.zeros((gameState.numCols, gameState.numRows))
+
+                def diffuse(self):
+                    for n in xrange(self.numDiffusions):
+                        # mode='same' means the output array should be the same size
+                        # bounary='wrap' means to wrap the convolution around the array dimensions
+                        # self.diffusionAry = sig.convolve2d(self.diffusionAry, diffusionKernel, mode='same', boundary='wrap')
+                        #                                   * self.obstacleAry
+                        for i in (-1, 1):
+                            for j in (0, 1):
+                                #experimentation needed here, for efficiency.
+                                self.array += (diffuseAmt*np.roll(self.array*self.gameState.obstacleAry, i, j))
+                                self.array = ary * self.gameState.obstacleAry
+                                # convolution is unbounded, so scale the array to range [0, 1]
+                                max = np.max(self.array)
+                                if max > 0:
+                                    self.array /= max
+                                            
+	def __init__(self, mapFile):
 		"""Constructs a new GameState object"""
-		pass
+                # load obstacle array from map file
+                self.obstacleAry = np.loadtxt(mapFile, dtype='c').astype('float')
+                # how many columns and rows are in this map?  (it is assumed that maps are rectangular)
+                self.numCols = self.obstacleAry.shape[0]
+                self.numRows = self.obstacleAry.shape[1]
+                # instantiate EntityGroups
+                self.entityGroups["Bird"] = EntityGroup(self, "Bird", (Metric(self, "attract",  .3, 30),
+                                                                       Metric(self, "repulse", -.1, 30))
+                self.entityGroups["Food"] = EntityGroup(self, "Food", (Metric(self, "attract",  .5, 30)))
 
 	def update(self):
 		"""Applies diffusion to all environment metrics.  Also calls update on all entity groups"""
