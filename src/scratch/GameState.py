7aaa45fe67b90b3782b705188f12abcdb2bcd414
Dustin Schmidt <xianthrops@gmail.com>
2012-11-16 14:53:12 -0800

    added a third iteration in the folder src/scratch.  also scratch.py shows some plots with temporal decays

---
+++ src/scratch/GameState.py
@@ -0,0 +1,298 @@
+import Tkinter as tk
+import numpy as np
+import Image
+import ImageTk
+import diffuse
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm, Colormap
+
+
+class Hawk():
+	def __init__(self, size):
+		self.alive = True
+		self.str = "H"
+		self.canvasItemId = -1
+		im = Image.open("hawk.png")
+		im = im.resize(size)
+		self.image = ImageTk.PhotoImage(image=im)
+	def getMove(self, neighborhood):
+		#import pdb;pdb.set_trace()
+		maxAt = np.argmax(neighborhood['Bird'])
+		moves = [(-1,-1),
+				 (-1,0),
+				 (-1,1),
+				 (0,-1),
+				 (0,0),
+				 (0,1),
+				 (1,-1),
+				 (1,0),
+				 (1,1)]
+		return moves[maxAt]
+	def getMetrics(self):
+		return {'Hawk':10,'HawkRepulse':2}
+
+class Food():
+	def __init__(self, size):
+		self.alive = True
+		self.str = "F"
+		self.canvasItemId = -1
+		im = Image.open("apple.png")
+		im = im.resize(size)
+		self.image = ImageTk.PhotoImage(image=im)
+	def getMove(self, neighborhood):
+		return (0,0)
+	def getMetrics(self):
+		return {'Food':10}
+
+class Bird():
+	weights = {'Food':{'weight':1,'dir':1},
+			   'Bird':{'weight':0,'dir':1},
+			   'Hawk':{'weight':0,'dir':1},
+			   'BirdRepulse':{'weight':0,'dir':1}}
+	def __init__(self, size):
+		self.alive = True
+		self.str = "B"
+		self.canvasItemId = -1
+		self.skill = 10
+		im = Image.open("bird.png")
+		im = im.resize(size)
+		self.image = ImageTk.PhotoImage(image=im)
+	def getMove(self, neighborhood):
+		a = {}
+		maxVal = 0
+		maxLayer = None
+		for k, v in self.weights.items():
+			a[k] = v['weight']*neighborhood[k]
+			m = np.max(a)
+			if m>maxVal:
+				maxVal = m
+				maxLayer = k
+
+		dir = self.weights[maxLayer]['dir']
+		maxAt = np.argmax(neighborhood['Food'])
+		moves = {1:[(-1,-1),
+				 (-1,0),
+				 (-1,1),
+				 (0,-1),
+				 (0,0),
+				 (0,1),
+				 (1,-1),
+				 (1,0),
+				 (1,1)],
+				 -1:[(1,1),
+				     (1,0),
+				     (1,-1),
+				     (0,1),
+				     (0,0),
+				     (0,-1),
+				     (-1,1),
+				     (-1,0),
+				     (-1,-1)]}
+		coords = np.nonzero(neighborhood['entities'])
+		for row, col in zip(coords[0], coords[1]):
+			if isinstance(neighborhood['entities'][row,col],Food):
+				#global kill
+				#kill = True
+				#maxAt = (row,col)
+				neighborhood['entities'][row,col] = None
+				self.skill +=10
+
+		return moves[dir][maxAt]
+
+	def getMetrics(self):
+		return {'Bird':self.skill,'BirdRepulse':2}
+
+class Game(tk.Frame):
+	def __init__(self, master=tk.Tk(), height=900, width=900):
+		tk.Frame.__init__(self, master)
+		self.master = master
+		self.deltaT = 1 #:Time delay in ms between frame updates, not guaranteed
+		self.paused = False
+		plt.ion()
+		self.showPlot = True
+		self.plotVal = "Food"
+		self.height=height
+		self.width=width
+		self.loadMap()
+		self.createWidgets()
+		self.pack()
+		self.initEntities()
+		self.initObstacles()
+		self.initMetrics()
+		self.update()
+		#self.plot()
+		self.draw()
+
+	def createWidgets(self):
+		self.Surface = tk.Canvas(self, width=self.width, height = self.height, bg="#FFFFFF")
+		self.Surface.bind("<Button-1>", self.leftClick)
+		self.Surface.bind("<Button-3>", self.rightClick)
+		self.master.bind("<Key>",self.keyPress)
+		self.Surface.pack()
+
+	def loadMap(self, mapFile='map1.map'):
+		noneFunc = lambda size: None
+		entityIDMapping = {'E':noneFunc,'F':Food,'B':Bird,'H':Hawk}
+		gameMap = np.loadtxt(mapFile,dtype='c')
+		self.shape = gameMap.shape
+		self.entities = np.empty(gameMap.shape,dtype=object)
+		self.obstacles = np.ones(gameMap.shape)
+		self.cellsize = self.cellToPixel(1,1)
+		self.cellsize = (int(self.cellsize[0]), int(self.cellsize[1]))
+		for row in range(self.shape[0]):
+			for col in range(self.shape[1]):
+				if gameMap[row,col] == 'O':
+					self.obstacles[row,col] = 0
+				else:
+					self.entities[row,col] = entityIDMapping[gameMap[row,col]](self.cellsize)
+
+	def cellToPixel(self, row, col):
+		"""Returns tuple (x,y) representing the upper left corner of the given cell in pixel coordinates"""
+		return (col*float(self.width)/self.shape[1], row*float(self.height)/self.shape[0])
+
+	def pixelToCell(self,x,y):
+		"""returns tuple (row,col) representing the cell which contains the given x,y coordinate"""
+		return (int(y*self.shape[0]/float(self.height)),int(x*self.shape[1]/float(self.width)))
+	
+	def initEntities(self):
+		coords = np.nonzero(self.entities)
+		for row, col in zip(coords[0], coords[1]):
+			img = self.Surface.create_image(self.cellToPixel(row,col), image=self.entities[row,col].image, anchor="nw")
+			self.entities[row,col].canvasItemId = img
+		self.Surface.update()
+
+	def initObstacles(self):
+		neighborCoeff = self.sumOfNeighbors(self.obstacles)
+		self.neighborCoeff = neighborCoeff + np.logical_not(neighborCoeff)
+		self.neighborCoeff = self.obstacles / self.neighborCoeff
+		coords = np.nonzero(np.logical_not(self.obstacles))
+		for row, col in zip(coords[0], coords[1]):
+			self.Surface.create_rectangle(self.cellToPixel(row,col), self.cellToPixel(row+1,col+1), fill="#000000")
+		self.Surface.update()
+
+	def initMetrics(self):
+		self.metrics={'Hawk'		:{'rate':0.9,'iters':30,'seed':np.zeros(self.shape),'diffused':np.zeros(self.shape)},
+					  'HawkRepulse' :{'rate':0.1,'iters':30,'seed':np.zeros(self.shape),'diffused':np.zeros(self.shape)},
+					  'Bird'		:{'rate':0.9,'iters':30,'seed':np.zeros(self.shape),'diffused':np.zeros(self.shape)},
+					  'BirdRepulse' :{'rate':0.1,'iters':30,'seed':np.zeros(self.shape),'diffused':np.zeros(self.shape)},
+					  'Food'		:{'rate':0.9,'iters':30,'seed':np.zeros(self.shape),'diffused':np.zeros(self.shape)}}
+		
+
+	def seedMetrics(self):
+		for name, data in self.metrics.items():
+			self.metrics[name]['seed'].fill(0)
+		coords = np.nonzero(self.entities)
+		for row, col in zip(coords[0], coords[1]):
+			entMetrics = self.entities[row,col].getMetrics()
+			for k,v in entMetrics.items():
+				self.metrics[k]['seed'][row,col] = v
+
+	def diffuseMetrics(self):
+		for name, data in self.metrics.items():
+			seed = data['seed']
+			rate = data['rate']
+			itr = data['iters']
+			diff = data['diffused']
+			mask = np.logical_not(seed)
+			#self.metrics[name]['diffused'] = diffuse.diffuse(itr, rate,
+            #                         seed, self.obstacles)
+			#import pdb; pdb.set_trace()
+			for i in range(itr):
+				diff = rate*self.neighborCoeff*self.sumOfNeighbors(diff)*mask + seed
+			self.metrics[name]['diffused']=diff
+	def sumOfNeighbors(self, a):
+		new = np.zeros(a.shape)
+		new = np.roll(a,1,0)+np.roll(a,1,1)+np.roll(a,-1,0)+np.roll(a,-1,1)
+		return new
+
+
+	def getNeighborhood(self,row,col):
+		neighborhood = {}
+		neighborhood['entities'] = self.entities[row-1:row+2, col-1:col+2]
+		neighborhood['obstacles'] = self.obstacles[row-1:row+2, col-1:col+2]
+		for layer, data in self.metrics.items():
+			neighborhood[layer] = data['diffused'][row-1:row+2, col-1:col+2]
+		return neighborhood
+	def update(self):
+		#self.initMetrics()
+		self.seedMetrics()
+		self.diffuseMetrics()
+		coords = np.nonzero(self.entities)
+		global kill
+		for row, col in zip(coords[0], coords[1]):
+			if self.entities[row,col] == None or not self.entities[row,col].alive:
+				continue
+			n = self.getNeighborhood(row,col)
+			move = self.entities[row,col].getMove(n)
+			newPos = ((row+move[0])%self.entities.shape[0], (col+move[1])%self.entities.shape[1])
+			if self.entities[newPos] == None or self.entities[newPos].alive==False:
+				self.entities[newPos] = self.entities[row, col]
+				self.entities[row,col] = None
+			#if kill:
+			#	pass#self.paused=True
+		if self.showPlot: self.plot()
+		self.draw()
+		#plt.draw()
+
+	def draw(self):
+		coords = np.nonzero(self.entities)
+		for row, col in zip(coords[0], coords[1]):
+			item = self.entities[row,col].canvasItemId
+			pos = self.cellToPixel(row,col)
+			self.Surface.coords(item, pos)
+		self.Surface.update()
+		if not self.paused:
+			self.Surface.after(self.deltaT,self.update)
+		
+
+	def plot(self):
+		#if self.plt = None:
+		plt.figure(0)
+		plt.clf()
+
+		plt.suptitle(self.plotVal)
+		m = self.metrics[self.plotVal]['diffused']
+		#lmin = np.min(m)
+		#from math import fabs
+		#n = fabs(lmin)*np.ones(m.shape)
+		ln = LogNorm()
+		#m = n + m
+		#ln.autoscale(m)
+		plt.imshow(m,interpolation='none', norm=ln)
+		plt.contour(m,norm=ln,colors='black', linewidth=.5)
+		#plt.imshow(m,interpolation='none')
+		#plt.contour(m,colors='black', linewidth=.5)
+		#plt.canvas.draw()
+		plt.show()
+
+	def leftClick(self, event):
+		row, col = self.pixelToCell(event.x, event.y)
+		if self.entities[row, col] == None:
+			self.entities[row,col] = Food(self.cellsize)
+			img = self.Surface.create_image(self.cellToPixel(row,col), image=self.entities[row,col].image, anchor="nw")
+			self.entities[row,col].canvasItemId = img
+		# if self.paused:
+		# 	self.update()
+			
+		# 	fCount = 0
+		# 	for Name, data in self.metrics.items():
+		# 		pass
+				
+	def keyPress(self, event):
+		print "KeyPress"
+		if event.char == "p":
+			self.showPlot = not self.showPlot
+		elif event.char == "f":
+			self.plotVal = "Food"
+		elif event.char ==  "b":
+			self.plotVal = "Bird"
+		elif event.char == "h":
+			self.plotVal = "Hawk"
+
+
+	def rightClick(self, event):
+		self.paused = not self.paused
+		self.draw()
+
+
+g = Game()
---
+++ src/scratch/diffuse.c
@@ -0,0 +1,122 @@
+#include "Python.h"
+#include "arrayobject.h"
+
+static PyObject *diffuse(PyObject *self, PyObject *args);
+
+static char module_docstring[] =
+    "This module provides an interface for calculating diffusion using C.";
+static char diffusion_docstring[] =
+    "Return the diffused array calculated from a 2D input array and obstacle array.";
+
+static PyMethodDef module_methods[] = {
+    {"diffuse", diffuse, METH_VARARGS, diffusion_docstring},
+    {NULL, NULL}
+};
+
+PyMODINIT_FUNC initdiffuse(void) {
+    Py_InitModule3("diffuse", module_methods, module_docstring);
+    /* Load `numpy` functionality. */
+    import_array();
+}
+
+/* Create Carray from PyArray
+ * Assumes PyArray is contiguous in memory.
+ * Memory is allocated!
+ */
+double **doubleMatrixToCArrayPtrs(PyArrayObject *arrayin, int numCols, int numRows)  {
+    double **c, *a;
+    int i;
+    // Allocate a double *vector (vec of pointers)
+    c = (double **)malloc(numCols * sizeof(double));
+    a = (double *)arrayin->data;  /* pointer to arrayin data as double */
+    for (i = 0; i < numCols; i++)  {
+        c[i] = a + i * numRows;
+    }
+    return c;
+}
+
+int **intMatrixToCArrayPtrs(PyArrayObject *arrayin, int numCols, int numRows)  {
+    int **c, *a;
+    int i;
+    // Allocate a double *vector (vec of pointers)
+    c = (int **)malloc(numCols * sizeof(int));
+    a = (int *)arrayin->data;  /* pointer to arrayin data as double */
+    for (i = 0; i < numCols; i++)  {
+        c[i] = a + i * numRows;
+    }
+    return c;
+}
+
+/*
+ * Diffuse a 2d numpy array using the given number of iterations, rate,
+ * metric array and obstacle array.
+ 
+ * Usage: newMetricArray = diffuse(int numIterations, float rate,
+ *                                 2dNpAry metricArray, 2dNpAry obstacleAry)
+ */
+static PyObject *diffuse(PyObject *self, PyObject *args) {
+    PyArrayObject *metricArray, *obstacleArray, *resultArray;
+    double rate;
+    double **cMetricArray, **cResultArray;
+    int **cObstacleArray;
+    int numIterations, i, left, right, up, down, col, row, numCols, numRows;
+    int dimensions[2];
+
+    // parse metric and obstacle arrays and check return value
+    if (!PyArg_ParseTuple(args, "idO!O!", &numIterations, &rate,
+                          &PyArray_Type, &metricArray,
+                          &PyArray_Type, &obstacleArray))
+        return NULL;
+    if (metricArray == NULL || obstacleArray == NULL)
+        return NULL;
+    numCols = metricArray->dimensions[0];
+    numRows = metricArray->dimensions[1];
+    dimensions[0] = numCols;
+    dimensions[1] = numRows;
+    resultArray = (PyArrayObject *)PyArray_FromDims(2, dimensions, NPY_DOUBLE);
+    cMetricArray = doubleMatrixToCArrayPtrs(metricArray, numCols, numRows);
+    cObstacleArray = intMatrixToCArrayPtrs(obstacleArray, numCols, numRows);
+    cResultArray = doubleMatrixToCArrayPtrs(resultArray, numCols, numRows);
+
+    // copy metric array into result array
+    for (col = 0; col < numCols; col++) {
+        for (row = 0; row < numRows; row++) {
+            cResultArray[col][row] = cMetricArray[col][row];
+        }
+    }
+    
+    for (i = 0; i < numIterations; i++) {
+        for (col = 0; col < numCols; col++) {
+            left = col - 1 < 0 ? numCols - 1 : col - 1;
+            right = col + 1 >= numCols ? 0 : col + 1;
+            for (row = 0; row < numRows; row++) {
+                // if this cell is not a goal or obstacle, diffuse
+                if (cObstacleArray[col][row] && cMetricArray[col][row] < 1.0) {
+                    up = row - 1 < 0 ? numRows - 1 : row - 1;
+                    down = row + 1 >= numRows ? 0 : row + 1;
+                    // final diffusion value for col, row is the sum of neighbors times diffusion rate
+                    cResultArray[col][row] = rate * (cResultArray[left][row] +
+                                                     cResultArray[right][row] +
+                                                     cResultArray[col][up] +
+                                                     cResultArray[col][down]);
+                }
+            }
+        }
+
+        // zero out obstacle cells in each iteration
+        for (col = 0; col < numCols; col++) {
+            for (row = 0; row < numRows; row++) {
+                if (!cObstacleArray[col][row]) {
+                    cResultArray[col][row] = 0;
+                }
+            }
+        }
+    }
+
+    // free allocated memory
+    free((char*)cMetricArray);
+    free((char*)cObstacleArray);
+    free((char*)cResultArray);
+    
+    return PyArray_Return(resultArray);
+}
